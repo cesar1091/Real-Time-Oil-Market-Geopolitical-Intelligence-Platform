63261d52ac3844e13cd499126663d8988d88f274
Aaron Fernandez <cesarfernandezn@bcp.com.pe>
2026-05-24 13:12:46 -0500

    Update libraries from app

--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@
 from flask import Flask, request, render_template
 from database.db import getDatabase, getNewsTable, getOilTable, getSentimentTable
 import spacy
-from transformers import pipeline
 
 TICKERS = ['CL=F', 'BZ=F', 'NG=F']
 app = Flask(__name__, static_folder="static", template_folder="templates")
