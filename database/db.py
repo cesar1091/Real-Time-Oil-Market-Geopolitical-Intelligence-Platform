bbf5b04beffe7687febc38b2152d7fc8d1329501
Aaron Fernandez <cesarfernandezn@bcp.com.pe>
2026-05-24 11:15:02 -0500

    Update database

--- database/db.py
+++ database/db.py
@@ -1,6 +1,19 @@
 from tinydb import TinyDB
+import os
+import json
 
 def getDatabase(path: str = "data/scraping_data.json") -> TinyDB:
+
+    # Create file if doesn't exist
+    if not os.path.exists(path):
+        with open(path, "w") as f:
+            json.dump({}, f)
+
+    # Fix empty file
+    if os.path.getsize(path) == 0:
+        with open(path, "w") as f:
+            json.dump({}, f)
+
     return TinyDB(path)
 
 def getNewsTable(db: TinyDB) -> TinyDB.table:
